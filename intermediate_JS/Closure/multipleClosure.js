14295a265cc7f461b11b425ad53a889581351b00
nirvanafan205 <nirvanafan205@gmail.com>
2023-01-25 00:50:59 -0800

    comments

--- intermediate_JS/Closure/multipleClosure.js
+++ intermediate_JS/Closure/multipleClosure.js
@@ -19,3 +19,22 @@ myNewFunction();
 const anotherFunction = out();
 anotherFunction();
 anotherFunction();
+
+/*
+    If we run 'outer again and store the returned 'incrementCounter' function
+    definition in 'anotherFunction', this new incrementCounter function was created in 
+    a new execution context and therefore has a brand new indepened backpack
+
+*/
+
+/*
+    Closure gives our functions persistent memories and entirely new toolkit for writing professional code
+
+        Helper functions: 'once' and 'memorize'
+
+        iterators and generators: Which use lexical scoping and closure to achieve the most contemporary patterns for handling data in JavaScript
+
+        Module pattern: Preserve state for the life of an application without polluting the globabl namespace
+
+        Asynchronous JavaScript: Callbacks and Promises rely on closure to persist state in an asynchronous environment
+*/
