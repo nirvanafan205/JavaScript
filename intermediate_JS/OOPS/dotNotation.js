ba3557db59893937317970d229a289f84f1acfba
nirvanafan205 <nirvanafan205@gmail.com>
2023-01-27 15:47:18 -0800

    intro to OOPS

---
+++ intermediate_JS/OOPS/dotNotation.js
@@ -0,0 +1,38 @@
+// Classes, Prototypes - Object Oriented JavaScript
+
+/*
+    An enormously popular paradigm for structuring our complex code
+
+    Prototype chain - the feature behind the scene that eneables emulation of OOP but is a compelling tool in itself
+
+    Understanding the difference between __proto__ and prototype
+
+    The new and class keywords as tools to automate our object and method creation
+*/
+
+// Core of development (and running code)
+
+/*
+    Save data (in a quiz game that scores of user1 and user2)
+    Run code (functions) using that data (increase user 2's score)
+
+    In a quiz game I need to save lots of users, but also admis, quiz questions, quiz
+    outcomes, league tables - all have data and associated functionality
+
+    In 100,000 lines of code
+
+        Where is the functionality when I need it?
+        How do I make sure the functionality is only used on the right data!
+*/
+
+//Code should be
+
+/*
+    Easy to reason about
+
+    Easy to add features to (new functionality)
+
+    Efficient and performant
+
+    The Object-oriented paradigm aims is to let us achieve these three goals
+*/
