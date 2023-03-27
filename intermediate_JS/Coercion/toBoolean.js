6c06e6b9f1c7fead978e262ff7875c161772d4a6
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-03-27 12:22:17 -0700

    falsy and truthy

---
+++ intermediate_JS/Coercion/toBoolean.js
@@ -0,0 +1,32 @@
+// toBoolean checks for falsy and truthy
+
+// falsy: values that will return false when coereced with a boolean
+
+"" //empty string
+
+0, -0
+
+null
+
+NaN
+
+false
+undefined
+
+
+//truthy
+
+"foo"
+
+23
+
+{a: 1}
+
+[1, 3]
+
+true
+
+function() {...}
+
+
+// empty array would be truthy
