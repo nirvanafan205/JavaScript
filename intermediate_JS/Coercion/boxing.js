b89d9b5a4fc3ac6b7dc9a41629e6316ab4122935
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-03-27 13:44:54 -0700

    boxed

---
+++ intermediate_JS/Coercion/boxing.js
@@ -0,0 +1,8 @@
+// accessing properties on primitive values
+
+// how to access .length of some string value
+// it is accessed with boxing aka implicit coercion
+if(studentNameElem.value.length > 50)
+{
+    console.log("Students name too long.");
+}
